70f19535f7053b374bac5d381c660640d029c19e
karthikchintapalli <karthik.chintapalli@gmail.com>
2017-03-27 19:55:43 +0530

    First commit

---
+++ Assets/BallScript.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var parentbone:GameObject;
+var rigid:Rigidbody;
+function Start () {
+	transform.parent=parentbone.transform;
+	rigid.useGravity=false;
+}
+
+function Update () {
+	
+}
+function Release(){
+	transform.parent=null;
+	rigid.useGravity=true;
+	transform.rotation=parentbone.transform.rotation;
+	rigid.AddForce(transform.forward * -20000);
+
+}
---
+++ Assets/Thrower.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var theball:GameObject;
+function Start () {
+	
+}
+function Update(){
+}
+
+function ThrowBall () {
+	var ballscript:BallScript=theball.GetComponent("BallScript");
+	ballscript.Release();
+}
